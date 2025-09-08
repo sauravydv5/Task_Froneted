4b4b5a33bda47d11c7f97249b220ce466e6f3126
Saurav Kumar <sauravkumar91937@gmail.com>
2025-09-08 10:17:37 +0530

    update api url

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4001" });
-// const API = axios.create({ baseURL: "https://task1-1-fc4k.onrender.com" });
+// const API = axios.create({ baseURL: "http://localhost:4001" });
+const API = axios.create({ baseURL: "https://task1-1-fc4k.onrender.com" });
 
 // Attach JWT token automatically
 API.interceptors.request.use((req) => {
